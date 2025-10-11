cee5a63bd182d74f64b185fe50cb3b367cd1c2eb
Abel Kashoba <lebakashoba@hotmail.fr>
2025-10-11 21:56:59 +0100

    second commit

--- wp-config.php
+++ wp-config.php
@@ -75,7 +75,7 @@ $table_prefix = 'wp_';
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
 if ( ! defined( 'WP_DEBUG' ) ) {
-	define( 'WP_DEBUG', true ); 
+	define( 'WP_DEBUG', false ); 
     define( 'WP_DEBUG_LOG', true ); 
     define( 'WP_DEBUG_DISPLAY', false ); 
 }
