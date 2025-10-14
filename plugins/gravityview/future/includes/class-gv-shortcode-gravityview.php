6ea1f52c3b5c18f2a6bbdc9890515326ae5c4bda
Abel Kashoba <lebakashoba@hotmail.fr>
2025-10-14 13:19:41 +0100

    Model

--- plugins/gravityview/future/includes/class-gv-shortcode-gravityview.php
+++ plugins/gravityview/future/includes/class-gv-shortcode-gravityview.php
@@ -322,7 +322,7 @@ error_log("HAS ERROR ".json_encode($error));
 
 				$request = $mock_request;
 			}
-error_log("Choose laned ".json_encode($view));
+error_log("Choose laned ");
 			$renderer = new \GV\View_Renderer();
 			return self::_return( $renderer->render( $view, $request ) );
 		}
