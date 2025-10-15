f558958fd5cd7240ac83830b71cc29ffe830ce98
Abel Kashoba <lebakashoba@hotmail.fr>
2025-10-15 13:52:11 +0100

    Model

--- js/anser_gravity_test.js
+++ js/anser_gravity_test.js
@@ -1,141 +1,141 @@
-function Anser_loader(offset=0,page_size=10, search_term=''){
-    let url = new URL(GravityFlowAjax.ajax_url),
-    searchParams = url.searchParams;
-    
-    searchParams.set('action', GravityFlowAjax.action);
-    searchParams.set('security', GravityFlowAjax.nonce);
-    searchParams.set('offset', offset);
-    searchParams.set('page_size', page_size);
-    
-    if(search_term){
-        searchParams.set('term', search_term);
+var __commonJS = (cb, mod) => () => (mod || cb((mod = { exports: {} }).exports, mod), mod.exports);
+
+// js/anser_utily.js
+var require_anser_utily = __commonJS((exports) => {
+  function Anser_loader(offset = 0, page_size = 10, search_term = "") {
+    let url = new URL(GravityFlowAjax.ajax_url), searchParams = url.searchParams;
+    searchParams.set("action", GravityFlowAjax.action);
+    searchParams.set("security", GravityFlowAjax.nonce);
+    searchParams.set("offset", offset);
+    searchParams.set("page_size", page_size);
+    if (search_term) {
+      searchParams.set("term", search_term);
     }
-    
-    return fetch(url, { method:'POST'});
-}
-
-var loader = document.querySelector('#loader'),
-nextPage = document.querySelector('.nextPage'),
-prevPage = document.querySelector('.previousPage'),
-search_form = document.querySelector('.search_block');
-
-function display_nativation_handler(page,total){
-    if(page == 0){
-        prevPage.style.display = 'none';
+    return fetch(url, { method: "POST" });
+  }
+  exports.page_handler = function page_handler(navigationHandler) {
+    let nextPage = document.querySelector(".nextPage"), prevPage = document.querySelector(".previousPage"), loader = document.querySelector("#loader");
+    this.page = 0;
+    this.total_page = 0;
+    this.limit = 15;
+    if (nextPage && prevPage) {
+      nextPage.addEventListener("click", (event) => {
+        this.goTo(this.page + 1).then((json_response) => {
+          if (navigationHandler) {
+            navigationHandler(json_response);
+          }
+        });
+      });
+      prevPage.addEventListener("click", (event) => {
+        this.goTo(this.page - 1).then((json_response) => {
+          if (navigationHandler) {
+            navigationHandler(json_response);
+          }
+        });
+      });
+    } else {
+      console.error("NAVIGATION ELEMENT WERE NOT FOUND");
     }
-    else{
-        prevPage.style.display = 'inline';
+    function toggle_disable(value) {
+      nextPage.disabled = prevPage.disabled = value;
     }
-    
-    if(page >= (total -1)){
-        nextPage.style.display = 'none';
+    this.goTo = (newPage) => {
+      toggle_disable(true);
+      this.load_data(newPage * 10).then((json_response) => {
+        this.page = newPage;
+        console.log("THE NEW PAGE IS", this.page);
+        display_nativation_handler(newPage, this.total_page);
+        return json_response;
+      }).finally(() => {
+        toggle_disable(false);
+      });
+    };
+    this.load_data = function(offset = this.page, limit = this.limit, search_term = "") {
+      display("Chargements des données...");
+      return Anser_loader(offset, limit, search_term).then((response) => response.json()).then((response) => {
+        this.total_page = Math.ceil(response.data.total / this.limit);
+        this.page = offset;
+        display_nativation_handler(offset, this.total_page);
+        return response;
+      }).finally(() => {
+        display("");
+      });
+    };
+    function display_nativation_handler(page, total) {
+      if (page == 0) {
+        prevPage.style.display = "none";
+      } else {
+        prevPage.style.display = "inline";
+      }
+      if (page >= total - 1) {
+        nextPage.style.display = "none";
+      } else {
+        nextPage.style.display = "inline";
+      }
     }
-    else{
-        nextPage.style.display = 'inline';
+    function display(text) {
+      loader.textContent = text;
     }
-}
+  };
+});
 
-function page_handler(){
-	this.page = 0;
-	this.total_page = 0;
-	this.limit = 15;
+// js/anser_flow_utils.js
+var require_anser_flow_utils = __commonJS((exports) => {
+  exports.result_handler = function result_handler(json_response) {
+    let { entries, field_values } = json_response;
+    update_entries_ids(entries, field_values);
+    build_elements(entries);
+  };
+  exports.update_entries_ids = function update_entries_ids(entries, field_values) {
+    entries.forEach((entry) => {
+      let form_id = entry.form_id, form = field_values[form_id];
+      if (form) {
+        Object.keys(form).forEach((key) => {
+          let field_id = form[key];
+          entry[key] = entry[field_id];
+          delete entry[field_id];
+        });
+      } else {
+        console.error("No form_id " + form_id + " found in field_values");
+      }
+    });
+  };
+  exports.build_elements = function build_elements(entries) {
+    var html = "", tbody = document.querySelector("tbody");
+    if (tbody) {
+      entries.forEach((entry) => {
+        html += "<tr>";
+        html += "<td>" + entry.created_by + "</td>";
+        html += "<td>" + entry.workflow_step + "</td>";
+        html += "<td>" + entry["numéro"] + "</td>";
+        html += "<td>" + entry["objet"] + "</td>";
+        html += "<td>" + entry["référence"] + "</td>";
+        html += "</tr>";
+      });
+      tbody.innerHTML = html;
+    } else {
+      console.error("TBODY NOT FOUND");
+    }
+  };
+});
 
-	nextPage.addEventListener('click',(event)=>{
-		this.goTo(this.page + 1);
-	})
-
-	prevPage.addEventListener('click',(event)=>{
-		this.goTo(this.page - 1);
-	})
-
-	search_form.addEventListener('submit',(event)=>{
-		event.preventDefault();
-
-		let form = event.target,
-		input = form.elements.s;
-
-		if(input.value.length){
-			this.load_data(0,this.limit,input.value).then(()=>{
-				this.page = 0;
-				display_nativation_handler(this.page, this.total_page);
-			})
-		}
-		else{
-			console.warn("Nothing to search for");
-		}
-	})
-
-	function toggle_disable(value){
-		nextPage.disabled = prevPage.disabled = value;
-	}
-
-	this.goTo = (newPage)=>{
-		toggle_disable(true);
-
-		this.load_data(newPage * 10).then(()=>{
-			this.page = newPage;
-
-			console.log("THE NEW PAGE IS",this.page);
-
-			display_nativation_handler(newPage, this.total_page);
-		}).finally(()=>{
-			toggle_disable(false);
-		})
-	}
-
-
-	this.load_data = function(offset=this.page,limit=this.limit, search_term=''){
-		display("Chargements des données...");
-		return Anser_loader(offset,limit, search_term).then((response)=> response.json()).then((response)=>{
-			this.update_entries_ids(response.data.entries, response.data.field_values);
-			this.build_elements(response.data.entries);
-			this.total_page = Math.ceil(response.data.total / this.limit);
-		}).finally(()=>{
-			display("");
-		})
-	}
-
-	this.update_entries_ids = (entries,field_values)=>{
-		entries.forEach((entry)=>{
-			let form_id = entry.form_id,
-			form = field_values[form_id];
-
-			if(form){
-				Object.keys(form).forEach((key)=>{
-					let field_id = form[key];
-					entry[key] = entry[field_id];
-					delete entry[field_id];
-				})
-			}
-			else{
-				console.error("No form_id "+ form_id +" found in field_values")
-			}
-		})
-	}
-
-	this.build_elements = (entries)=>{
-		var html = "",
-		tbody = document.querySelector('tbody');
-
-		if(tbody){
-			entries.forEach((entry)=>{
-				html += "<tr>";
-				html += "<td>" + entry.created_by + "</td>";
-				html += "<td>" + entry.workflow_step + "</td>";
-				html += "<td>" + entry['numéro'] + "</td>";
-				html += "<td>" + entry['objet'] + "</td>";
-				html += "<td>" + entry['référence'] + "</td>";
-				html += "</tr>";
-			})
-
-			tbody.innerHTML = html;
-		}
-		else{
-			console.error("TBODY NOT FOUND");
-		}
-	}
-}
-
-function display(text){
-	loader.textContent = text;
-}
+// js/anser_gravity.js
+var require_anser_gravity = __commonJS((exports) => {
+  var { page_handler } = require_anser_utily();
+  var { result_handler } = require_anser_flow_utils();
+  var myPage_handler = new page_handler(result_handler);
+  var search_form = document.querySelector(".search_block");
+  search_form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let form = event.target, input = form.elements.s;
+    if (input.value.length) {
+      let limit = myPage_handler.limit;
+      myPage_handler.load_data(0, limit, input.value).then(result_handler);
+      exports.load_data(0, exports.limit, input.value).then(result_handler);
+    } else {
+      console.warn("Nothing to search for");
+    }
+  });
+  myPage_handler.load_data().then(result_handler);
+});
+export default require_anser_gravity();
--- js/anser_view_test.js
+++ js/anser_view_test.js
@@ -7,3 +7,41 @@ function data_loader(limit=10,offset=0){
 
 	return fetch(url,{ method:'GET' })
 }
+
+function display_data(entries){
+	let trs = "",
+	tbody = document.querySelector('tbody');
+
+	if(tbody){
+		entries.forEach((entry)=>{
+			trs += "<tr>";
+
+			for(let name in entry){
+				let value = entry[name];
+				trs += "<td>";
+
+				if(value.indexOf('http') == -1){
+					trs += value;
+				}
+				else{
+					let values = JSON.parse(value);
+					values.forEach((value)=>{
+						trs += "<a href='"+value+"'>";
+						let name = value.slice(value.lastIndexOf('/') + 1);
+						trs += name;
+
+						trs += "</a>";
+					})
+				}
+
+				trs += "</td>";
+			}
+			trs += "</tr>";
+		})
+
+		tbody.innerHTML = trs;
+	}
+	else{
+		console.error("No tbody found");
+	}
+}
