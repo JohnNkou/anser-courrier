1e2ab718a419db92cd1d5e14b7f1a9609daa45d1
Abel Kashoba <lebakashoba@hotmail.fr>
2025-10-15 12:59:06 +0100

    Model

--- js/anser_view_test.js
+++ js/anser_view_test.js
@@ -1,7 +1,9 @@
-function data_loader(){
+function data_loader(limit=10,offset=0){
 	var url = new URL(GravityViewAjax.ajax_url);
 	url.searchParams.set('action', GravityViewAjax.action);
 	url.searchParams.set('nonce', GravityViewAjax.nonce);
+	url.searchParams.set('limit',limit);
+	urls.searchParams.set('offset',offset);
 
 	return fetch(url,{ method:'GET' })
 }
