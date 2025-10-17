8bfff29722113ed8f07c6fa4d9a44b49d073aaaf
Abel Kashoba <lebakashoba@hotmail.fr>
2025-10-17 17:32:52 +0100

    Pushed

--- plugins/gravityview/future/includes/class-gv-shortcode-gravityview.php
+++ plugins/gravityview/future/includes/class-gv-shortcode-gravityview.php
@@ -264,6 +264,7 @@ class gravityview extends \GV\Shortcode {
 
 			$entryset = $entry->is_multi() ? $entry->entries : array( $entry );
 			error_log("Entry to view is ".json_encode($entry));
+			error_log("Entry class ".get_class($entry));
 			error_log("Entryset is ".json_encode($entryset));
 			foreach ( $entryset as $e ) {
 				if ( 'active' != $e['status'] ) {
