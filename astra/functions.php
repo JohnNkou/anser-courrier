de1386a8177770d7211734d971b09251c8df381f
Abel Kashoba <lebakashoba@hotmail.fr>
2025-10-21 20:45:21 +0100

    Pushed

--- astra/functions.php
+++ astra/functions.php
@@ -596,13 +596,15 @@ function load_gravityflow_inbox_entry(){
 }
 
 function build_inbox_results($form,$entry,$current_step){
+    require_once ABSPATH . "wp-content/plugins/gravityflow/includes/pages/class-entry-detail.php";
+
     $results = [];
     $current_index = 0;
     $display_empty_fields = false;
     $is_assignee = $current_step ? $current_step->is_user_assignee() : false;
     $complete_step = gravity_flow()->get_workflow_complete_step($form->ID, $entry);
 
-    error_log("ABSPATH IS ". ABSPATH);
+    error_log("PLUGIN DIR IS ". WP_PLUGIN_DIR);
 
     if(! $is_assignee){
         if($current_step){
