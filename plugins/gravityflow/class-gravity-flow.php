463832dd22040458c9ed5799416d455033fb67bc
Abel Kashoba <lebakashoba@hotmail.fr>
2025-10-23 15:53:16 +0100

    Pushed

--- plugins/gravityflow/class-gravity-flow.php
+++ plugins/gravityflow/class-gravity-flow.php
@@ -5662,7 +5662,7 @@ PRIMARY KEY  (id)
 					return;
 				}
 			}
-
+			error_log("Original a ".print_r($a));
 			$entry_id = absint( rgget( 'lid' ) );
 			error_log("GOT A ENTRY ID $entry_id");
 			if ( empty( $entry_id ) && ! empty( $a['entry_id'] ) ) {
