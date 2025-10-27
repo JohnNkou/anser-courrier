21fe50963b862bd5ae97220443fa31c6836a7ee2
Abel Kashoba <lebakashoba@hotmail.fr>
2025-10-27 15:46:05 +0100

    Modified

--- js/anser_flow_test.js
+++ js/anser_flow_test.js
@@ -281,8 +281,8 @@ var require_anser_flow_utils = __commonJS((exports2) => {
   function build_dependent_classe(rules) {
     return rules.map((rule) => "dependent_" + rule.fieldId).join(" ");
   }
-  function get_field_by_location(location2, inboxes) {
-    let indexes = location2.split(","), field = indexes.length == 2 && inboxes[indexes[0]][indexes[1]];
+  function get_field_by_location(_location, inboxes) {
+    let indexes = _location.split(","), field = indexes.length == 2 && inboxes[indexes[0]][indexes[1]];
     return field;
   }
   function check_validity({ form, field_ids, inboxes, required }) {
