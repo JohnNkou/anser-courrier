60dded1e4df6c557966afe8bad0c2ec7307ec6df
Abel Kashoba <lebakashoba@hotmail.fr>
2025-10-27 19:58:46 +0100

    Modified

--- astra/functions.php
+++ astra/functions.php
@@ -1131,7 +1131,6 @@ function handle_non_editable_field($form,$entry,$current_step,$field,$display_em
             $label = Gravity_Flow_Entry_Detail::get_label($field, $entry);
              if($display_empty_fields || ! empty($display_value) || $display_value === '0'){
                 $result['value'] = $display_value;
-                $result['label'] = $display_value;
                 return $result;
             }
             
