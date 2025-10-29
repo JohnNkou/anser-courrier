65ce7585f08da04084d8cdd0903972c58d686a84
Abel Kashoba <lebakashoba@hotmail.fr>
2025-10-29 15:17:01 +0100

    Modified

--- astra/functions.php
+++ astra/functions.php
@@ -595,7 +595,6 @@ function load_gravityview(){
             $id = $entry->ID;
         }
         $an['id'] = $id;
-        $an['form_id'] = $field->form_id;
         array_push($results, $an);
     }
 
