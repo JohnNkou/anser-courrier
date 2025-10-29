105e860d766b012090f7581fb0049c00bcce55cf
Abel Kashoba <lebakashoba@hotmail.fr>
2025-10-29 13:38:21 +0100

    Modified

--- astra/functions.php
+++ astra/functions.php
@@ -554,8 +554,8 @@ function load_gravityview(){
 
             add_filter('gravityview_search_criteria', function($criteria,$form_id,$view){
                 flogs("criteria %s", print_r($criteria,true));
-                flogs("form_id %s", print_r($form_id));
-                flogs("view %s", print_r($view));
+                flogs("form_id %s", print_r($form_id,true));
+                flogs("view %s", print_r($view,true));
                 return $criteria;
 
             },10,3);
