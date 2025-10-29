adb1de3b5c7f8964b15bbbe4bd9ede2e2374af56
Abel Kashoba <lebakashoba@hotmail.fr>
2025-10-29 15:27:14 +0100

    Modified

--- plugins/gravityview-multiple-forms/src/MultipleForms/Query.php
+++ plugins/gravityview-multiple-forms/src/MultipleForms/Query.php
@@ -839,7 +839,7 @@ class Query extends GF_Query {
 		GFCommon::log_debug( __METHOD__ . '(): sql => ' . $sql );
 
 		$this->timer_start();
-		$results          = $wpdb->get_results( $sql, ARRAY_N );
+		$results          = $wpdb->get_results( $sql, ARRAY_N ); flogs("SQL IS %s",$sql);
 		$this->queries [] = [ $this->timer_stop(), $sql ];
 
 		if ( is_null( $results ) ) {
