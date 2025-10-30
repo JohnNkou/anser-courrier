2efdd362f95cba49bd2dee3cc03dbe7377b7763b
Abel Kashoba <lebakashoba@hotmail.fr>
2025-10-30 10:17:36 +0100

    Modified

--- astra/functions.php
+++ astra/functions.php
@@ -350,11 +350,11 @@ require_once ABSPATH ."anser-php/anser_gravityflow_inbox.php";
 
 add_shortcode("anser_gravityview", array('Anser_Gravityview','shortcode'));
 
-error_log("WP_HEAD HAS BEEN CALLED 1 ".did_action("wp_head"));
+error_log("WP_HEAD HAS BEEN CALLED 1 ".did_action("gravityflow_enqueue_admin_scripts"));
 
 $anser_gf = Anser_GravityFlow::get_instance();
 
-error_log("WP_HEAD HAS BEEN CALLED 2 ".did_action("wp_head"));
+error_log("WP_HEAD HAS BEEN CALLED 2 ".did_action("gravityflow_enqueue_admin_scripts"));
 
 add_shortcode('anser_gravityflow', array($anser_gf, 'shortcode'));
 
