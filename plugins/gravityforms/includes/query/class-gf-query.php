7c2c593428d5628b3d514fcc9ac3b6b9056c61ee
Abel Kashoba <lebakashoba@hotmail.fr>
2025-10-31 07:28:08 +0100

    Updated

--- plugins/gravityforms/includes/query/class-gf-query.php
+++ plugins/gravityforms/includes/query/class-gf-query.php
@@ -816,7 +816,7 @@ class GF_Query {
 		GFCommon::log_debug( __METHOD__ . '(): sql => ' . $sql );
 
 		$this->timer_start();
-		$results = $wpdb->get_results( $sql, ARRAY_N ); flogs("SQLA %s",$sql);
+		$results = $wpdb->get_results( $sql, ARRAY_N );
 		$this->queries []= array( $this->timer_stop(), $sql );
 		
 		if ( is_null( $results ) ) {
