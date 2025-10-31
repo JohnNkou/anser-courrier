ca8ff8c88109f5d3459405a742662148c1311e3c
Abel Kashoba <lebakashoba@hotmail.fr>
2025-10-31 14:27:41 +0100

    Updated

--- astra/anser-php/utils.php
+++ astra/anser-php/utils.php
@@ -521,13 +521,6 @@ function get_upload_data_settings($html){
 function build_inbox_editable_result($form,$entry,$current_step){
     require_once ABSPATH . "/wp-content/plugins/gravityflow/includes/pages/class-entry-editor.php";
 
-    if(!Gravity_Flow_Entry_Detail::is_permission_granted($entry,$form,$current_step)){
-    	flogs("NO PERMISSION GRANTED PALL");
-    }
-    else{
-    	flogs("PERSMISSION GRANTED");
-    }
-
     $results = [[]];
     $current_index = 0;
     $entry_editor = new Gravity_Flow_Entry_Editor( $form, $entry, $current_step, 0 );
@@ -672,6 +665,13 @@ function build_inbox_editable_result($form,$entry,$current_step){
 
 function build_inbox_results($form,$entry,$current_step){
     require_once ABSPATH . "/wp-content/plugins/gravityflow/includes/pages/class-entry-detail.php";
+
+    if(!Gravity_Flow_Entry_Detail::is_permission_granted($entry,$form,$current_step)){
+    	flogs("NO PERMISSION GRANTED PALL");
+    }
+    else{
+    	flogs("PERSMISSION GRANTED");
+    }
 
     $results = [[]];
     $current_index = 0;
