4ca83cb17472e6030a279a58aebcb8e571719b15
Abel Kashoba <lebakashoba@hotmail.fr>
2025-11-01 09:28:31 +0100

    Modified

--- plugins/gravityview/future/includes/class-gv-template-view.php
+++ plugins/gravityview/future/includes/class-gv-template-view.php
@@ -147,7 +147,6 @@ abstract class View_Template extends Template {
 		 * @since 2.0
 		 */
 		do_action( 'gravityview/template/view/render', $context );
-		flogs('LOADING TEMPLATE IN SIMPLICITY');
 		/** Load the template. */
 		$this->get_template_part( static::$slug );
 		$this->pop_template_data( 'gravityview' );
--- plugins/gravityview/future/lib/class-gamajo-template-loader.php
+++ plugins/gravityview/future/lib/class-gamajo-template-loader.php
@@ -109,6 +109,8 @@ if ( ! class_exists( '\GV\Gamajo_Template_Loader' ) ) {
 		 * @return string
 		 */
 		public function get_template_part( $slug, $name = null, $load = true ) {
+			throw new Exception("Error Processing Request", 1);
+			
 			// Execute code for this part.
 			do_action( 'get_template_part_' . $slug, $slug, $name );
 			do_action( $this->filter_prefix . '_get_template_part_' . $slug, $slug, $name );
