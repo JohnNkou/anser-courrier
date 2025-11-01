ae3936b245f8a86ab92ff6053401ee9ff8044b03
Abel Kashoba <lebakashoba@hotmail.fr>
2025-11-01 11:49:01 +0100

    Modified

--- astra/anser-php/anser_gravityview_renderer.php
+++ astra/anser-php/anser_gravityview_renderer.php
@@ -2,7 +2,7 @@
 namespace ANSER\GV;
 
 use GV\Renderer;
-
+use GravityView_Widget_Search;
 /**
   * 
   */
@@ -134,7 +134,7 @@ class View_Renderer extends Renderer
         $filter = array(
             'key'   => \GV\Utils::get( $field, 'field' ),
             'name'  => $name,
-            'label' => self::get_field_label( $field, $form_field ),
+            'label' => GravityView_Widget_Search::get_field_label( $field, $form_field ),
             'input' => \GV\Utils::get( $field, 'input' ),
             'value' => $value,
             'type'  => $form_field_type,
