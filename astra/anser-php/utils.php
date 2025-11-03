2a9b585018107f210dcd8441a7d6905cb741ca4a
Abel Kashoba <lebakashoba@hotmail.fr>
2025-11-03 21:36:59 +0100

    Updated

--- astra/anser-php/utils.php
+++ astra/anser-php/utils.php
@@ -24,6 +24,7 @@ function process_download_file($permission_granted, $form_id, $field_id){
         ]);
 
         flogs('UPLOAD KEY IS %s',$upload_key);
+        flogs('S3 BUCKET %s', ADVMO_AWS_BUCKET);
 
         try{
             $result = $s3Client->getObject([
