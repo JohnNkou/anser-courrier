b9950efc4764e881260739a8939cd897a718c417
Abel Kashoba <lebakashoba@hotmail.fr>
2025-11-03 22:13:57 +0100

    Updated

--- astra/functions.php
+++ astra/functions.php
@@ -328,7 +328,7 @@ add_shortcode("anser_gravityflow",function(...$atts){
 
 add_filter("gform_entry_field_value","handle_gravity_form_submission",10,4);
 
-add_filter("gform_permission_granted_pre_download","process_download_file",10,3);
+#add_filter("gform_permission_granted_pre_download","process_download_file",10,3);
 
 $gravityflow_ajax_endpoint = GRAVITYFLOW_AJAX_ENDPOINT;
 $gravityview_ajax_endpoint = GRAVITYVIEW_AJAX_ENDPOINT;
