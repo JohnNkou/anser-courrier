56bf7734bce1bcf59d79281ab3673092b2d9a505
Abel Kashoba <lebakashoba@hotmail.fr>
2025-11-03 22:11:38 +0100

    Updated

--- astra/anser-php/utils.php
+++ astra/anser-php/utils.php
@@ -31,7 +31,7 @@ function process_download_file($permission_granted, $form_id, $field_id){
         try{
             $result = $s3Client->getObject([
                 "Bucket" => ADVMO_AWS_BUCKET,
-                "Key" => "medias/gravity_forms/14-16d0b2584492cee7559662fa423bf96d/2025/11/IMG_0169.jpeg"
+                "Key" => $upload_key
             ]);
 
             flogs("GOT THE RESULT");
