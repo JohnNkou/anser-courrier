01249c8cba5b0f781089f0ed24308aa8e78ee14c
Abel Kashoba <lebakashoba@hotmail.fr>
2025-11-04 09:05:50 +0100

    Updated

--- astra/anser-php/utils.php
+++ astra/anser-php/utils.php
@@ -81,7 +81,7 @@ function handle_gravity_form_submission($display_value, $field, $entry, $form ){
     if($field->type == 'fileupload'){
         $dir = wp_upload_dir();
         if(strpos($display_value, $dir['baseurl']) !== false){
-            $new_value = preg_replace("/". $dir['baseurl'] ."/", S3_BUCKET_URL, $display_value);
+            $new_value = str_replace($dir['baseurl'], S3_BUCKET_URL, $display_value);
             flogs("new IS %s %s", $new_value, $display_value);
         }
     }
