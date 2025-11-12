8c97d65ea5ae15a3f3f06b0cfa4f4ca2d5715ff9
Abel Kashoba <lebakashoba@hotmail.fr>
2025-11-12 22:39:28 +0100

    Updated

--- plugins/gp-nested-forms/includes/class-gpnf-session.php
+++ plugins/gp-nested-forms/includes/class-gpnf-session.php
@@ -128,8 +128,7 @@ class GPNF_Session {
 	}
 
 	public function set_cookie() {
-		throw new Exception("Error Processing Request", 1);
-		
+		flogs("SETTINGS COOKIE BONAVAN");
 		$result = setcookie( $this->get_cookie_name(), json_encode( $this->_cookie ), time() + 60 * 60 * 24 * 7, COOKIEPATH, COOKIE_DOMAIN, is_ssl() );
 	}
 
