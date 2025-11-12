56a75e5009c0a5b9b1bb5f9b71c538998f9ef0bc
abel-hostinger <abel@anser-hostinger.com>
2025-11-12 11:40:58 +0000

    Added the gp-nested-forms plugins

---
+++ plugins/gp-nested-forms/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.2.12',
+        'version' => '1.2.12.0',
+        'reference' => '3456749d1afb2fe9bbf280714147cdde24e6f62b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.2.12',
+            'version' => '1.2.12.0',
+            'reference' => '3456749d1afb2fe9bbf280714147cdde24e6f62b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
