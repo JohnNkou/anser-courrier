ce08710686cd1656664ee487386f1a0a91838129
Abel Kashoba <lebakashoba@hotmail.fr>
2025-12-16 14:21:49 +0100

    Updated

--- js/anser_flow_test.js
+++ js/anser_flow_test.js
@@ -683,7 +683,7 @@ var require_anser_flow_utils = __commonJS((exports2) => {
             let div = document.createElement("div"), label = document.createElement("label"), select = document.createElement("select"), div_error2 = document.createElement("div");
             if (inbox.leaf_value) {
               if (inbox.value.indexOf(inbox.leaf_value) != -1) {
-                inbox.value = inbox.value.replace(inbox.leaf_value, inbox.leaf_value + " selected");
+                inbox.value = inbox.value.replace(inbox.leaf_value, inbox.leaf_value + '" selected');
               }
             }
             label.textContent = inbox.label;
--- js/anser_flow_utils.js
+++ js/anser_flow_utils.js
@@ -347,7 +347,7 @@ function build_entry_element({ inbox, inputAtts, atts, failedAtts, inbox_index, 
 
         	if(inbox.leaf_value){
         		if(inbox.value.indexOf(inbox.leaf_value) != -1){
-        			inbox.value = inbox.value.replace(inbox.leaf_value,inbox.leaf_value + " selected");
+        			inbox.value = inbox.value.replace(inbox.leaf_value,inbox.leaf_value + '" selected');
         		}
         	}
 
