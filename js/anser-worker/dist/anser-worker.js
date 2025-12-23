5188c0acef6f8da65a106a5a418dc4baadc4f6d9
Abel Kashoba <lebakashoba@hotmail.fr>
2025-12-23 18:42:32 +0100

    Updated

--- js/anser-worker/dist/anser-worker.js
+++ js/anser-worker/dist/anser-worker.js
@@ -1,5 +1,5 @@
 // js/anser-worker/index.js
-var APP_NAME = "anser-worker-v1.1.1";
+var APP_NAME = "anser-worker-v1.1.2";
 var COOKIE_NAME = "u-e";
 self.addEventListener("install", (event) => {
   self.skipWaiting();
@@ -11,8 +11,8 @@ self.addEventListener("message", (event) => {
   }
   console.log("RECEIVED MESSAGE", data);
   if (type == "REGISTER") {
-    let sKeys = [];
-    url.searchParams.forEach((value, key) => {
+    let sKeys = [], local_url = new URL(url.href);
+    local_url.searchParams.forEach((value, key) => {
       if (key != cookie_name) {
         sKeys.push(key);
       }
@@ -26,11 +26,11 @@ self.addEventListener("message", (event) => {
     }).finally(() => {
       console.log("Going after the duck");
       caches.open(APP_NAME).then((cache) => {
-        cache.match(url.href).then((response) => {
+        cache.match(local_url).then((response) => {
           if (!response) {
             fetch(url).then((response2) => {
               if (response2.status == status) {
-                cache.put(url.href, response2);
+                cache.put(local_url, response2);
                 event.source.postMessage("URL " + url + " successfully cached");
               } else {
                 console.log("STATUS DIFFERENT THEN THE GIVEN", response2, status);
--- js/anser-worker/index.js
+++ js/anser-worker/index.js
@@ -1,4 +1,4 @@
-const APP_NAME = 'anser-worker-v1.1.1',
+const APP_NAME = 'anser-worker-v1.1.2',
 COOKIE_NAME = 'u-e';
 
 self.addEventListener('install',(event)=>{
@@ -19,8 +19,9 @@ self.addEventListener('message',(event)=>{
 	console.log('RECEIVED MESSAGE', data);
 
 	if(type == 'REGISTER'){
-		let sKeys = [];
-		url.searchParams.forEach((value,key)=>{
+		let sKeys = [],
+		local_url = new URL(url.href);
+		local_url.searchParams.forEach((value,key)=>{
 			if(key != cookie_name){
 				sKeys.push(key);
 			}
@@ -37,11 +38,11 @@ self.addEventListener('message',(event)=>{
 			console.log("Going after the duck");
 
 			caches.open(APP_NAME).then((cache)=>{
-				cache.match(url.href).then((response)=>{
+				cache.match(local_url).then((response)=>{
 					if(!response){
 						fetch(url).then((response)=>{
 							if(response.status == status){
-								cache.put(url.href,response);
+								cache.put(local_url,response);
 
 								event.source.postMessage("URL "+url + " successfully cached");
 							}
