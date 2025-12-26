cffbc5812f209c50363ad303f986ac66a0561577
Abel Kashoba <lebakashoba@hotmail.fr>
2025-12-26 19:12:06 +0100

    Updated

--- js/anser-worker/dist/anser-worker.js
+++ js/anser-worker/dist/anser-worker.js
@@ -1,5 +1,5 @@
 // js/anser-worker/index.js
-var APP_NAME = "anser-worker-v1.1.10";
+var APP_NAME = "anser-worker-v1.1.11";
 var COOKIE_NAME = "u-e";
 self.addEventListener("install", (event) => {
   self.skipWaiting();
--- js/anser-worker/index.js
+++ js/anser-worker/index.js
@@ -1,4 +1,4 @@
-const APP_NAME = 'anser-worker-v1.1.10',
+const APP_NAME = 'anser-worker-v1.1.11',
 COOKIE_NAME = 'u-e';
 
 self.addEventListener('install',(event)=>{
