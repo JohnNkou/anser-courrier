49abd1776d66e8c5773d7705e73ec202007e4a5e
Abel Kashoba <lebakashoba@hotmail.fr>
2025-12-30 17:58:40 +0100

    Updated

--- astra/anser-php/utils.php
+++ astra/anser-php/utils.php
@@ -1348,4 +1348,8 @@ function search_reception($term, $offset=0,$limit=15){
     return ["entries"=>[], "total"=>0];
 }
 
+add_filter("option_active_plugins",function($plugins){
+    flogs("PLUGINS ARE %s",$plugins);
+});
+
 ?>
