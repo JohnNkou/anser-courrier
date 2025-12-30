069da26f31e5fbf89337bd179a06bdb3713c9e99
Abel Kashoba <lebakashoba@hotmail.fr>
2025-12-30 16:30:22 +0100

    Updated

---
+++ astra/anser-php/profiler.php
@@ -0,0 +1,34 @@
+<?php
+	/**
+	 * This class will be used for profiling
+	 */
+	class Profiler
+	{
+		private $names = [];
+		private $prefix;
+
+		function __construct($prefix = ""){
+			$this->prefix = $prefix;
+		}
+		
+		public function time($keyName)
+		{
+			$names[$keyName] = hrtime(true);
+		}
+
+		public function timeEnd($keyName){
+			$timeThen = $names[$keyName] ?? null;
+			if ($timeThen) {
+				$timeElapsed = (hrtime(true) - $timeThen) / 1e+6;
+				error_log(sprintf("\n\n%s It took %sms to run %s\n\n", $this->prefix, $timeElapsed, $keyName ));
+
+				return;
+			}
+
+			error_log("No timeStart found in names for keyName ".$keyName);
+
+			return null;
+		}
+	}
+
+?>
