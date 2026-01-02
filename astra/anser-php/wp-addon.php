0b8bcb4a761781dfcc2fa48bf850bed2e3ae3d26
Abel Kashoba <lebakashoba@hotmail.fr>
2026-01-02 12:41:27 +0100

    Updated

--- astra/anser-php/wp-addon.php
+++ astra/anser-php/wp-addon.php
@@ -11,7 +11,7 @@
 		        	GRAVITYFLOW_AJAX_ENDPOINT,
 		        	GRAVITYFLOW_ENTRY_AJAX_ENDPOINT
 		        ])){
-		        	$authorized_plugins = ['gravityforms.php','gravityflow.php','gravityview.php','gp-', 'gravityformswebhooks','gravityformsadvancedpostcreation','gp-nested-forms'];
+		        	$authorized_plugins = ['gravityform','gravityflow','gravityview.php','gp-', 'gravityformswebhooks','gravityformsadvancedpostcreation'];
 
 		        	if($method == 'POST'){
 		        		array_push($authorized_plugins, 'gravityflow');
