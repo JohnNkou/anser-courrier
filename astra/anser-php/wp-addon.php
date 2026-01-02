1fe97ec3dca4280ec1e3d6a695e350b39dd3bed7
Abel Kashoba <lebakashoba@hotmail.fr>
2026-01-02 12:24:16 +0100

    Updated

--- astra/anser-php/wp-addon.php
+++ astra/anser-php/wp-addon.php
@@ -3,46 +3,43 @@
 		$displayed = false;
 		add_filter('option_active_plugins', function($plugins) use(&$displayed){
 			if(isset($_REQUEST['action'])){
-				if(true/*$_SERVER['REQUEST_METHOD'] == 'GET'*/){
-					require_once "constant.php";
-			        $authorized_plugins;
+				$method = $_SERVER['REQUEST_METHOD'];
+				require_once "constant.php";
+		        $authorized_plugins;
 
-			        if(in_array($_REQUEST['action'], [
-			        	GRAVITYFLOW_AJAX_ENDPOINT,
-			        	GRAVITYFLOW_ENTRY_AJAX_ENDPOINT
-			        ])){
-			        	$authorized_plugins = ['gravityform','gravityflow','gravityview.php', 'gravity-notifications.php'];
-			        }
-			        elseif (in_array($_REQUEST['action'], [
-			        	GRAVITYVIEW_AJAX_ENDPOINT, GRAVITYVIEW_ENTRY_AJAX_ENDPOINT
-			        ])) {
-			        	$authorized_plugins = ['gravity'];
-			        }
-			        
-			        if(isset($authorized_plugins) && is_array($authorized_plugins)){
-			        	$plugins = array_filter($plugins,function($data) use($authorized_plugins){
-			               foreach ($authorized_plugins as $k_name){
-			                   	if(strpos($data,$k_name) !== false){
-			                   		return true;
-			                   	}
-			               } 
-			               
-			               return false;
-			            });
+		        if(in_array($_REQUEST['action'], [
+		        	GRAVITYFLOW_AJAX_ENDPOINT,
+		        	GRAVITYFLOW_ENTRY_AJAX_ENDPOINT
+		        ])){
+		        	$authorized_plugins = ['gravityform','gravityflow.php','gravityview.php', 'gravity-notifications.php'];
 
-			            if(!$displayed){
-			            	error_log(sprintf("Total plugin loaded after custom filter %s",count($plugins)));
-			            	error_log(sprintf("Plugin after filter %s",print_r($plugins,true)));
-			            	$displayed = true;
-			            }
-			        } 
-				}  
-				else{
-					if(!$displayed){
-						error_log("SKIPPING PLUGINS FILTER BECAUSE OF REQUEST METHOD NOT POST");
-						$displayed = true;
-					}
-				}
+		        	if($method == 'POST'){
+		        		array_push($authorized_plugins, 'gravityflow');
+		        	}
+		        }
+		        elseif (in_array($_REQUEST['action'], [
+		        	GRAVITYVIEW_AJAX_ENDPOINT, GRAVITYVIEW_ENTRY_AJAX_ENDPOINT
+		        ])) {
+		        	$authorized_plugins = ['gravity'];
+		        }
+		        
+		        if(isset($authorized_plugins) && is_array($authorized_plugins)){
+		        	$plugins = array_filter($plugins,function($data) use($authorized_plugins){
+		               foreach ($authorized_plugins as $k_name){
+		                   	if(strpos($data,$k_name) !== false){
+		                   		return true;
+		                   	}
+		               } 
+		               
+		               return false;
+		            });
+
+		            if(!$displayed){
+		            	error_log(sprintf("Total plugin loaded after custom filter %s",count($plugins)));
+		            	error_log(sprintf("Plugin after filter %s",print_r($plugins,true)));
+		            	$displayed = true;
+		            }
+		        }
 		    }
 		    /*elseif (strpos($_SERVER['REQUEST_URI'], 'reception-dg')) {
 		    	error_log(sprintf("TOTAL PLUGINS LOADED ARE %s", print_r($plugins,true)));
