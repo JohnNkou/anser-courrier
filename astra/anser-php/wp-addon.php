c28523da242e2028f0ef24d8f97e296846ef6625
Abel Kashoba <lebakashoba@hotmail.fr>
2026-01-02 15:35:10 +0100

    Updated

--- astra/anser-php/wp-addon.php
+++ astra/anser-php/wp-addon.php
@@ -6,7 +6,7 @@
 				$method = $_SERVER['REQUEST_METHOD'];
 				require_once "constant.php";
 		        $authorized_plugins;
-		        $banned_plugins = ["prevent-","user-","um-","rcp-","hostinger","say-what","tuxedo-big-file","wp-"];
+		        $banned_plugins = ["prevent-","user-","um-","rcp-","hostinger","say-what","tuxedo-big-file","wp-","elementor","wordpress"];
 
 		        $plugins = array_filter($plugins,function($plugin) use($banned_plugins){
 		        	
