de93df772a5d2511fade41bcbaea702ac1987ad0
Abel Kashoba <lebakashoba@hotmail.fr>
2026-01-02 18:39:19 +0100

    Updated

--- astra/anser-php/utils.php
+++ astra/anser-php/utils.php
@@ -425,7 +425,7 @@ function load_gravityview(){
         array_push($results, $an);
     }
 
-    $profiler->timeEnd("load_gravityview");
+    $profiler->timeEnd("Start");
 
     wp_send_json_success(["entries"=>$results, "total"=> $entries->total()]);
 }
