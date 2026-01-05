dcd50377f5ba185e1e574a595d19c39ad533a6ce
Abel Kashoba <lebakashoba@hotmail.fr>
2026-01-05 09:44:10 +0100

    Updated

--- astra/anser-php/wp-addon.php
+++ astra/anser-php/wp-addon.php
@@ -20,7 +20,7 @@
 		        elseif (in_array($_REQUEST['action'], [
 		        	GRAVITYVIEW_AJAX_ENDPOINT, GRAVITYVIEW_ENTRY_AJAX_ENDPOINT
 		        ])) {
-		        	$authorized_plugins = ['gravityview.php','gravityforms.php',/*'gravityview-diy','gravityview-entry-revisions','gravityview-advanced-filter',*/'gravityview-featured-entries','gravityview-multiple-forms','spellbook'];
+		        	$authorized_plugins = ['gravityview.php','gravityforms.php',/*'gravityview-diy','gravityview-entry-revisions',*/'gravityview-advanced-filter','gravityview-featured-entries',/*'gravityview-multiple-forms',*/'spellbook'];
 		        }
 
 		        if(isset($authorized_plugins) && is_array($authorized_plugins)){
